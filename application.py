bb7d3adf3e080fc83a4da547ceba8aa017e0936f
Sonik Kulichenko <sofiy.kulichenko@gmail.com>
2026-09-18 17:53:30 +0300

    fix: update greeting string identifier to 9ped0fct

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def index():
-    return "Hello from cmtr-9ped0fct Elastic Beanstalk CI-CD"
+    return "Hello from 9ped0fct Elastic Beanstalk CI-CD"
 
 if __name__ == "__main__":
     application.run(host="0.0.0.0", port=5000)
