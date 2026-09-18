bb619b0cbcaa6749abedf3fb9dc384e1955159ec
9ped0fct <9ped0fct@cloud-mentor.local>
2026-09-18 14:58:54 +0000

    Update application.py for verification

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def index():
-    return "Hello from 9ped0fct Elastic Beanstalk CI-CD"
+    return "Hello from 9ped0fct Elastic Beanstalk CI-CD verification-2026091814095432324"
 
 if __name__ == "__main__":
     application.run(host="0.0.0.0", port=5000)
